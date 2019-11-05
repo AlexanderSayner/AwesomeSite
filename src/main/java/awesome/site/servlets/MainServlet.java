dd43ca55f8270b48c669e67229cbc5ae0accafad
Alexander <alexandr.DEF@mail.ru>
2019-11-05 18:19:14 +0400

    Java has came

---
+++ src/main/java/awesome/site/servlets/MainServlet.java
@@ -0,0 +1,23 @@
+package awesome.site.servlets;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet
+public class MainServlet extends HttpServlet {
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        response.setContentType("text/html");
+        RequestDispatcher dispatcher = request.getRequestDispatcher("index.html");
+        if (dispatcher != null) {
+            dispatcher.forward(request, response);
+        }
+    }
+}
